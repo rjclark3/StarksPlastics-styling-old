8c647f16316ba0512b05ca2331ed2a9f8076617c
rjclark3 <44754316+rjclark3@users.noreply.github.com>
2019-06-13 12:51:58 -0400

    Add files via upload

---
+++ SawCutYield.py
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------------
+# Assignment Name: Saw Cut Yields
+# Name: Rodney Clark
+# -----------------------------------------------------------------
+import math
+import os
+
+intCount = int(0)
+
+#Gather inputs
+dblDimension1 = float(input("Enter Dimension 1 "))
+dblDimension2 = float(input("Enter Dimension 2 "))
+dblJointEdge1 = float(input("How many edge finished sides (Dimension 1) "))
+dblJointEdge2 = float(input("How many edge finished sides (Dimension 2)  "))
+
+# Calculate How much is needed for jointed edges
+dblJoint1 = float( dblJointEdge1 * .03125 )
+dblJoint2 = float( dblJointEdge2 * .03125 )
+
+# Calculate The Size Of Each cut after sawblade
+dblCutSize1 = (dblDimension1 + dblJoint1 + .125)
+dblCutSize2 = (dblDimension2 + dblJoint2 + .125)
+
+# Calculate The Yield if cut each direction
+dblYield1 = (96 / dblCutSize1) * (48 / dblCutSize2)
+dblYield2 = (96 / dblCutSize2) * (48 / dblCutSize1)
+
+# Print results
+print("Cut dimensions for this part are " + dblCutSize1 + " x " + dblCutSize2)
+print("The cut size of " + dblCutSize1 + " along the 96 yields " + dblYield1 + " pieces.")
+print("The cut size of " + dblCutSize2 + " along the 96 yields " + dblYield2 + " pieces.")
+
+#Compare Yield 1 to yield 2 and print result
+if ( dblYield1 > dblYield2 ):
+    print("The cut size of " + dblCutSize1 + " along the 96 yields is the better yield" )
+else:
+    print("The cut size of " + dblCutSize2 + " along the 96 yields is the better yield" )
+
+os.system("pause")
