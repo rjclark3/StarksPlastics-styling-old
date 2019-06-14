44865e8cdc1a61149e44c81619b9b958aeb9e1f0
rjclark3 <44754316+rjclark3@users.noreply.github.com>
2019-06-14 10:10:57 -0400

    Add files via upload
    Cleaned up some commenting and added the int type to the yields

--- SawCutYield.py
+++ SawCutYield.py
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------
-# Assignment Name: Saw Cut Yields
+# Project Name: Saw Cut Yields
 # Name: Rodney Clark
 # -----------------------------------------------------------------
 import math
@@ -11,7 +11,7 @@ intCount = int(0)
 dblDimension1 = float(input("Enter Dimension 1 "))
 dblDimension2 = float(input("Enter Dimension 2 "))
 dblJointEdge1 = float(input("How many edge finished sides (Dimension 1) "))
-dblJointEdge2 = float(input("How many edge finished sides (Dimension 2)  "))
+dblJointEdge2 = float(input("How many edge finished sides (Dimension 2) "))
 
 # Calculate How much is needed for jointed edges
 dblJoint1 = float( dblJointEdge1 * .03125 )
@@ -22,16 +22,16 @@ dblCutSize1 = (dblDimension1 + dblJoint1 + .125)
 dblCutSize2 = (dblDimension2 + dblJoint2 + .125)
 
 # Calculate The Yield if cut each direction
-dblYield1 = (96 / dblCutSize1) * (48 / dblCutSize2)
-dblYield2 = (96 / dblCutSize2) * (48 / dblCutSize1)
+intYield1 = int((96 / dblCutSize1) * (48 / dblCutSize2))
+intYield2 = int((96 / dblCutSize2) * (48 / dblCutSize1))
 
 # Print results
 print("Cut dimensions for this part are " + dblCutSize1 + " x " + dblCutSize2)
-print("The cut size of " + dblCutSize1 + " along the 96 yields " + dblYield1 + " pieces.")
-print("The cut size of " + dblCutSize2 + " along the 96 yields " + dblYield2 + " pieces.")
+print("The cut size of " + dblCutSize1 + " along the 96 yields " + intYield1 + " pieces.")
+print("The cut size of " + dblCutSize2 + " along the 96 yields " + intYield2 + " pieces.")
 
 #Compare Yield 1 to yield 2 and print result
-if ( dblYield1 > dblYield2 ):
+if ( intYield1 > intYield2 ):
     print("The cut size of " + dblCutSize1 + " along the 96 yields is the better yield" )
 else:
     print("The cut size of " + dblCutSize2 + " along the 96 yields is the better yield" )
